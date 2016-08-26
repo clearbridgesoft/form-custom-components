f7cca163719d56d2277ce8a4b9d7b1b9616ba475
link <esurllink@gmail.com>
2016-08-26 14:40:57 +1000

    project setup

---
+++ src/widgets/index.js
@@ -0,0 +1,11 @@
+import wysiwyg from './wysiwyg';
+import html from './html';
+import dateTime from './dateTime';
+import label from './label';
+
+export default {
+  wysiwyg,
+  html,
+  dateTime,
+  label
+};
